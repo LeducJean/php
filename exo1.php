bb3b940ea2bdd1a8f94eeb3f998a95c9ed57ca56
Jean Leduc <jean.leduc80@gmail.com>
2022-12-09 16:42:25 +0100

    exo1 finish

--- exo1.php
+++ exo1.php
@@ -3,14 +3,43 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
+    <title>Exo1</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
 <body>
 
- 
+
+
+
+
+<div>
+       
+        <?php  
+
+
+
+        $Variable = rand(0,100);
+
+
+            if($Variable%2==1){
+
+               echo "<div style='background-color:red;'>$Variable est impaire"; 
+
+            }else{
+
+                echo "<div style='background-color:blue;'>$Variable est paire"; 
+
+            }
+
+
+
+        ?>
+            
+    </div>
+    
+
 
 
 
